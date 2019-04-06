763a907b634d5fb208f4609ac834a5e20812cc68
三上大河 <mikamimikami60@gmail.com>
2019-04-06 19:11:34 +0900

    add message section add fecha add eslinter

--- rtsupport/components/App.jsx
+++ rtsupport/components/App.jsx
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
 import UserSection from './users/UserSection.jsx';
+import MessageSection from './messages/MessageSection.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       channels: [],
-      users: []
+      activeChannel:{name: 'Select your Channel'},
+      users: [],
+      messages: []
     };
   }
   addChannel(name) {
@@ -26,6 +29,15 @@ class App extends Component {
     this.setState({users});
     // TODO: Send to server
   }
+
+  addMessage (body) {
+    let {messages, users} = this.state;
+    let createdAt = new Date;
+    let author = users.length > 0 ? users[0].name : 'anonymous';
+    messages.push({id: messages.length, body, createdAt, author});
+    this.setState({messages});
+    // TODO: Send to server
+  }
   render() {
     return (
       <div className='app'>
@@ -40,6 +52,10 @@ class App extends Component {
             setUser={this.setUser.bind(this)}
           />
         </div>
+        <MessageSection
+          {...this.state}
+          addMessage={this.addMessage.bind(this)}
+        />
       </div>
     )
   }
