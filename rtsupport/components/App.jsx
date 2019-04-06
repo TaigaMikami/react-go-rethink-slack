f52d4c2b571bee0eb3da7b9be4691d8530f35280
三上大河 <mikamimikami60@gmail.com>
2019-04-06 15:33:58 +0900

    add rtsupport - finish channel section

---
+++ rtsupport/components/App.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import ChannelSection from './channels/ChannelSection.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channels: []
+    };
+  }
+  addChannel(name) {
+    let {channels} = this.state;
+    channels.push({id: channels.length, name});
+    this.setState({channels});
+    // TODO: Send to server
+  }
+  setChannel (activeChannel) {
+    this.setState({activeChannel});
+  }
+  render() {
+    return (
+      <div className='app'>
+        <div className='nav'>
+          <ChannelSection
+            {...this.state}
+            setChannel={this.setChannel.bind(this)}
+            addChannel={this.addChannel.bind(this)}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
